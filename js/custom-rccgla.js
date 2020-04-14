33af8870c2be63f250159ff81c821a2638793c43
rccgloveregina <63160811+rccgloveregina@users.noreply.github.com>
2020-04-14 01:26:32 +0100

    Add files via upload

---
+++ js/custom-rccgla.js
@@ -0,0 +1,404 @@
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1DDExu0nSLjqvP_5DCdBOWy3__BprsYa844eKQ4IWiFg/pubhtml';
+
+function init() {
+    Tabletop.init({
+        key: publicSpreadsheetUrl,
+        callback: showInfo
+       // order: 'event-date'
+       ///simpleSheet: true
+    });
+}
+
+function showInfo(data, tabletop) {
+    //alert('Successfully processe9d!');
+    var dataevent = tabletop.sheets('events').elements; 
+    var datagen = tabletop.sheets('general-info').elements;
+    var dataservces = tabletop.sheets('services').elements;
+    //console.log(dataservces);
+
+    for (var i = 0; i < datagen.length; i++) {
+        var address = datagen[i]['address'];
+        var country = datagen[i]['country'];
+        var email = datagen[i]['email'];
+        var tel = datagen[i]['telephone'];
+        var inst = datagen[i]['instagram-link'];
+        var fb = datagen[i]['facebook-link'];
+        var twitt = datagen[i]['twitter-link'];
+        var yout = datagen[i]['youtube-link'];
+
+        var praydesc = datagen[i]['prayer-description'];
+        var prayertagline = datagen[i]['prayer-tagline'];
+        var prayeraudio = datagen[i]['prayer-audio'];
+
+        var givedesc = datagen[i]['giving-description'];
+        var interacdesc = datagen[i]['interac-description'];
+        var contactdesc = datagen[i]['contact-description'];
+        var givingtagline = datagen[i]['giving-tagline'];
+
+        var mandatequote = datagen[i]['mandate-quote'];
+        var mandateauthor = datagen[i]['mandate-author'];
+
+
+        var aboutdesc = datagen[i]['about-description'];
+        var abouttagline = datagen[i]['about-tagline'];
+        var sitetitle = datagen[i]['site-title'] + ' ' + datagen[i]['city'] + ', ' + datagen[i]['country-abbr'];
+
+
+        var announcementtitle = datagen[i]['announcement-title'];
+        var announcementdescription = datagen[i]['announcement-description'];
+        var announcementtagline = datagen[i]['announcement-tagline'];
+        var announcementurl = datagen[i]['announcement-url'];
+        var announcementdate = datagen[i]['announcement-date'];
+        
+
+     
+
+        $('.announcementtitle').html(announcementtitle);
+        $('.announcementdescription').html(announcementdescription);
+        $('.announcementtagline').html(announcementtagline);
+        $('.announcementurl').html(announcementurl);
+        $('.announcementdate').html('Last updated: '+ announcementdate);
+
+
+
+       
+
+        $('.aboutdesc').html(aboutdesc);
+        $('.abouttagline').html(abouttagline);
+        $('.sitetitle').html(sitetitle);
+
+        $('.address').html(address);
+        $('#email').html(email);
+        $('#tel').html(tel);
+
+        $('.inst').attr('href', inst);
+        $('.fb').attr('href', fb);
+        $('.twitt').attr('href', twitt);
+        $('.yout').attr('href', yout);
+
+        $('#praydesc').html(praydesc);
+
+        $('#prayertagline').html(prayertagline);
+        $('.prayeraudio').attr('src', './images/prayer/' + prayeraudio);
+
+        $('#givedesc').html(givedesc);
+        $('.interacdesc').html(interacdesc);
+        $('#contactdesc').html(contactdesc);
+
+        $('#givingtagline').html(givingtagline);
+
+        $('.country').html(country);
+        $('#mandatequote').html(mandatequote);
+        $('#mandateauthor').html(mandateauthor);
+
+        
+    }
+
+    var soutput;
+    var soutput2;
+    var soutputW;
+    var soutputM;
+    var soutputO;
+    $('#usl-services').empty();
+    $('#services-sec').empty();
+    $('#usl-services-w').empty();
+    $('#usl-services-m').empty();
+    $('#usl-services-o').empty();
+    
+    var sdataservces = dataservces.filter(function (service) {
+        return service['days-of-week'] === 'Sunday' && service['recurring'] === 'Weekly';
+    });
+
+
+    var sdataservcesW = dataservces.filter(function (service) {
+        return service['recurring'] === 'Weekly' && service['days-of-week'] !== 'Sunday';
+    });
+
+    var sdataservcesM = dataservces.filter(function (service) {
+        return service['recurring'] === 'Monthly';
+    });
+
+    var sdataservcesO = dataservces.filter(function (service) {
+        return service['recurring'] === 'Other';
+    });
+
+    console.log(sdataservcesW);
+
+    for (var i = 0; i < sdataservces.length; i++) {
+      
+        soutput = '<li><span>' + sdataservces[i]['service-title'] + ': ' + sdataservces[i]['start-time'] + ' - ' + sdataservces[i]['end-time'] + '</span></li >';
+
+
+        soutput2 = '<h5 class="title color-black fw-b oo_mt co_mb ttu"><span>' + sdataservces[i]['service-title'] + ': ' + sdataservces[i]['start-time'] + ' - ' + sdataservces[i]['end-time'] + '</span></h5>';
+
+        $('#usl-services').append(soutput);
+        $('#services-sec').append(soutput2);
+        
+
+    }
+
+    for (var i = 0; i < sdataservcesW.length; i++) {
+
+        soutputW = '<li><span>' + sdataservcesW[i]['service-title'] + ': ' + sdataservcesW[i]['start-time'] + ' - ' + sdataservcesW[i]['end-time'] + ' every ' + sdataservcesW[i]['days-of-week'] + '</span></li >';
+
+        $('#usl-services-w').append(soutputW);
+
+    }
+
+
+    for (var i = 0; i < sdataservcesM.length; i++) {
+
+        soutputM = '<li><span>' + sdataservcesM[i]['service-title'] + ': ' + sdataservcesM[i]['start-time'] + ' - ' + sdataservcesM[i]['end-time'] + '</span><p>' + sdataservcesM[i]['service-description'] + '</p></li >';
+
+        $('#usl-services-m').append(soutputM);
+
+    }
+
+
+    for (var i = 0; i < sdataservcesO.length; i++) {
+
+        soutputO = '<li><span>' + sdataservcesO[i]['service-title'] + ': ' + sdataservcesO[i]['start-time'] + ' - ' + sdataservcesO[i]['end-time'] + '</span><p>' + sdataservcesO[i]['service-description'] + '</p></li >';
+
+        $('#usl-services-o').append(soutputO);
+
+    }
+
+
+
+
+
+
+
+
+    var evoutput;
+    $('#result-events').empty();
+   
+    var today = new Date();
+    var date = today.getTime();
+    
+    var edataevent = dataevent.filter(function (event) {
+        return new Date(event['event-date']).getTime() >= date;
+    });
+
+    if (edataevent.length === 0) {
+        $('#result-events').html("<p>There are no upcoming events to display at this time.</p>");
+    }
+
+    //console.log(edataevent);
+
+    var edataevent2 = edataevent.sort(function (a, b) {
+        return new Date(a['event-date']).getTime() - new Date(b['event-date']).getTime();
+    });
+
+   // console.log(edataevent2);
+
+    for (var i = 0; i < edataevent2.length; i++) {
+     
+        evoutput = '<div class="col-md-4 col-sm-6"> <a href="images/events/' + edataevent2[i]['event-image'] + '" style="padding:0px; margin:0px"><div class="card"> <img class="card-img-top" src="./images/events/' + edataevent2[i]['event-image'] + '" alt="Card image" style="width:100%;height:412px;margin-bottom:0"><div class="card-body"> <h4 class="card-title" >' + edataevent2[i]['event-title'] + '</h4><p class="card-text"><span class="fa fa-clock-o fa-fw" style="margin-right:10px"></span>' + edataevent2[i]['event-date'] + ' ' + edataevent2[i]['start-time'] + ' - ' + edataevent[i]['end-time'] + '</p><p class="card-text" > <span class="fa fa-map-o fa-fw" style="margin-right:10px"></span>' + edataevent2[i]['event-location'] + '</p> </div ></div ></a></div > ';
+        $('#result-events').append(evoutput);
+
+    }
+    
+    
+}
+
+window.addEventListener('DOMContentLoaded', init);
+
+
+
+function loadlatestevent(data, tabletop) {
+  
+   var data2 = tabletop.sheets('events').elements;
+    var evoutput;
+    $('#result-events').empty();
+    if (data2.length == 0) {
+        $('#result-events').html("<p>There are no upcoming events to display at this time.</p>");
+    }
+    //console.log(data2);
+   
+    for (var i = 0; i < data2.length; i++) {
+        var datecust = data2[i]['event-date'];
+        var datecust2 = data2[i]['event-date'];
+        var today = new Date();
+        var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+        alert(date);
+        alert(datecust2);
+
+
+
+
+        evoutput = '<div class="col-md-4 col-sm-6"> <a href="images/events/' + data2[i]['event-image'] + '" style="padding:0px; margin:0px"><div class="card"> <img class="card-img-top" src="./images/events/' + data2[i]['event-image'] + '" alt="Card image" style="width:100%;height:230px;margin-bottom:0"><div class="card-body"> <h4 class="card-title" >' + data2[i]['event-title'] + '</h4><p class="card-text"><span class="fa fa-clock-o fa-fw" style="margin-right:10px"></span>' + data2[i]['event-date'] + ' ' + data2[i]['start-time'] + ' - ' + data2[i]['end-time'] + '</p><p class="card-text" > <span class="fa fa-map-o fa-fw" style="margin-right:10px"></span>' + data2[i]['event-location'] + '</p> </div ></div ></a></div > ';
+        $('#result-events').append(evoutput);
+
+    }
+
+}
+
+
+
+function Sermons() {
+    document.getElementById("sermons").style.width = "100%";
+    $('#result-sermon').empty();
+    $('#result-sermon').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+    loadlatestsermon();
+}
+
+
+function Events() {
+    document.getElementById("events").style.width = "100%";
+    //$('#result-events').empty();
+    //$('#result-events').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+   // init();
+   // loadlatestevent();
+
+}
+
+function Media() {
+    document.getElementById("media").style.width = "100%";
+    //$('#result-events').empty();
+    //$('#result-events').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+    // init();
+    // loadlatestevent();
+
+}
+
+function Prayer() {
+    document.getElementById("pray").style.width = "100%";
+    //$('#result-events').empty();
+    //$('#result-events').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+    // init();
+    // loadlatestevent();
+
+}
+
+function Give() {
+    document.getElementById("give").style.width = "100%";
+    //$('#result-events').empty();
+    //$('#result-events').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+    // init();
+    // loadlatestevent();
+
+}
+
+
+function Contact() {
+    document.getElementById("contact").style.width = "100%";
+    //$('#result-events').empty();
+    //$('#result-events').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+    // init();
+    // loadlatestevent();
+
+}
+
+
+
+function Services() {
+    document.getElementById("services").style.width = "100%";
+    //$('#result-events').empty();
+    //$('#result-events').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+    // init();
+    // loadlatestevent();
+
+}
+
+function About() {
+    document.getElementById("about").style.width = "100%";
+    //$('#result-events').empty();
+    //$('#result-events').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+    // init();
+    // loadlatestevent();
+
+}
+
+function Announcement() {
+    document.getElementById("announcement").style.width = "100%";
+    //$('#result-events').empty();
+    //$('#result-events').html('<img src="images/photos/preloader.GIF"/>');
+    $('.push-body').css('overflow', 'hidden');
+    // init();
+    // loadlatestevent();
+
+}
+
+
+
+
+
+function closeNav() {
+    //document.getElementByClassName("overlaypages").style.width = "0%";
+    $(".overlaypages").css("width", "0%");
+    $(".push-body").css("overflow", "visible");
+}
+
+
+//Sermons Videos
+
+function loadlatestsermon() {
+
+    var channelId = 'UCNTiRBQNyfd64dGakU_Zg-Q';
+    var channelIds = 'UCHp4qCAPmz7-5BJ601FDFnA';
+    var channelName = '';
+    var pid;
+    var videoTitle;
+    var videoId;
+    var vidResult = 6;
+    var vidWidth = 387;
+    var vidHeight = 218;
+
+  //  $(document).ready(function () {
+        $.get(
+            "https://www.googleapis.com/youtube/v3/channels", {
+                part: 'contentDetails',
+                id: channelId,
+                key: 'AIzaSyB-PeMZY-XZo4yWigfWVhl8-D3bxptW1j4'
+            },
+            function (data) {
+                $.each(data.items, function (i, item) {
+                    console.log(item);
+                    pid = item.contentDetails.relatedPlaylists.uploads;
+                    getVids(pid);
+                });
+            }
+        );
+
+        function getVids() {
+            $.get(
+                "https://www.googleapis.com/youtube/v3/playlistItems", {
+                    part: 'snippet',
+                    maxResults: vidResult,
+                    playlistId: pid,
+                    key: 'AIzaSyB-PeMZY-XZo4yWigfWVhl8-D3bxptW1j4'
+                },
+                function (data) {
+                    var output;
+                    $('#result-sermon').empty();
+
+                    if (data.items.length == 0) {
+                        $('#result-sermon').html("<p>There are no sermons to display at this time.</p>");
+                    }
+                    $.each(data.items, function (i, item) {
+                        console.log(item);
+                        videoTitle = item.snippet.title;
+                        videoId = item.snippet.resourceId.videoId;
+                        output = '<div class="col-md-4 col-sm-6"><div class="embed-responsive embed-responsive-16by9"><iframe lass="embed-responsive-item" width="' + vidWidth + '" height="' + vidHeight + '" fullscreen src=\"//www.youtube.com/embed/' + videoId + '\" frameborder="0" allowfullscreen></iframe></div></div>';
+
+                        $('#result-sermon').append(output);
+
+                    });
+                }
+            );
+        }
+   // });
+
+}
+
+
+
---
+++ js/custom.js
@@ -0,0 +1,525 @@
+/**
+=============================================================
+[Custom Js]
+
+Template:     CharityGold
+Version:      1.0
+Created:      06.08.2017
+Primary use:  Charity and Donation HTML5 Template
+
+@Author: icute_theme
+=============================================================
+**/
+(function($) {
+  
+  "use strict";
+  
+  
+  // Preloader 
+  function stylePreloader() {
+    $('.preloader').delay(300).fadeOut(600);
+  } 
+
+  // Header Style
+  $('.header-main').scrollToFixed();
+
+  // Main Menu  
+  $(function() {
+    $(".mainmenu li a").each(function() {
+      if ($(this).next().length > 0) {
+        $(this).addClass("parent");
+      }
+    });
+    
+    $(".toggleMenu").on('click', function(e) {
+      e.preventDefault();
+      $(this).toggleClass("active");
+      $(".mainmenu").toggle();
+    });
+
+    adjustMenu();
+  });
+
+  var ww = document.body.clientWidth;
+  var adjustMenu = function() {
+    if (ww < 768) {
+      $(".toggleMenu").css("display", "inline-block");
+      if (!$(".toggleMenu").hasClass("active")) {
+        $(".mainmenu").hide();
+      } else {
+        $(".mainmenu").show();
+      }
+      $(".mainmenu li").unbind('mouseenter mouseleave');
+      $(".mainmenu li a.parent").unbind('click').bind('click', function(e) {
+        // must be attached to anchor element to prevent bubbling
+        e.preventDefault();
+        $(this).parent("li").toggleClass("dropdown-toggle");
+      });
+    } 
+    else if (ww >= 768) {
+      $(".toggleMenu").css("display", "none");
+      $(".mainmenu").show();
+      $(".mainmenu li").removeClass("dropdown-toggle");
+      $(".mainmenu li a").unbind('click');
+      $(".mainmenu li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+        // must be attached to li so that mouseleave is not triggered when hover over submenu
+        $(this).toggleClass('dropdown-toggle');
+      });
+    }
+  };
+
+  // jPushMenu
+  $('.toggle-menu').jPushMenu();
+
+  // Side Menu Dropdown
+  $(".sidemenu-dropdown").on('click', function () {
+      $('.sidemenu-dropdown-active').not($(this).children("ul")).hide();
+      $(this).children(".sidemenu-dropdown-active").slideToggle('fast');
+  });
+
+  // Owl Carousel
+
+    //client-slider
+    $('.client-slider').owlCarousel({
+        loop: true,
+        margin: 60,
+        dots: false,
+        nav: true,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+        ],
+        smartSpeed: 500,
+        autoplay: 2000,
+        items: 6,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 2,
+                loop: true
+            },
+            480: {
+                items: 3,
+                loop: true
+            },
+            600: {
+                items: 4,
+                loop: true
+            },
+            1000: {
+                items: 5,
+                loop: true
+            },
+            1200: {
+                items: 6,
+                loop: true
+            }
+        }
+    });
+
+    //testimonial-slider
+    $('.testimonial-slider').owlCarousel({
+        loop: true,
+        margin: 15,
+        dots: true,
+        nav: false,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+        ],
+        smartSpeed: 500,
+        autoplay: false,
+        items: 3,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                loop: true
+            },
+            480: {
+                items: 1,
+                loop: true
+            },
+            600: {
+                items: 2,
+                loop: true
+            },
+            1000: {
+                items: 2,
+                loop: true
+            },
+            1200: {
+                items: 3,
+                loop: true
+            }
+        }
+    });
+
+    //gallery-carousel
+    $('.gallery-carousel').owlCarousel({
+        loop:true,
+        margin:3,
+        dots: false,
+        nav:true,
+        autoplayHoverPause:false,
+        autoplay: 5000,
+        smartSpeed: 700,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+        ],
+        responsive:{
+          0:{
+            items:1
+          },
+          480:{
+            items:1
+          },
+          600:{
+            items:2
+          },
+          800:{
+            items:2
+          },
+          1024:{
+            items:3
+          },
+          1200:{
+            items:4
+          }
+        }
+    });
+
+    //event slider
+    $('.owl-column.event-list-slider').owlCarousel({
+        loop:true,
+        autoplay: false,
+        autoplayHoverPause:false,
+        smartSpeed: 700,
+        items: 3,
+        margin:30,
+        dots: true,
+        nav:true,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            683: {
+                items: 2
+            },
+            748: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
+    });
+
+    //owl-Carousel-OneColumn
+    $('.owl-column.owl-col-1').owlCarousel({
+        loop:true,
+        autoplay: 5000,
+        autoplayHoverPause:false,
+        smartSpeed: 700,
+        items: 1,
+        margin:15,
+        dots: true,
+        nav:true,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+        ]
+    });
+
+    //owl-Carousel-TwoColumn
+    $('.owl-column.owl-col-2').owlCarousel({
+        loop:true,
+        autoplay: 5000,
+        autoplayHoverPause:false,
+        smartSpeed: 700,
+        items: 2,
+        margin:30,
+        dots: true,
+        nav:true,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+        ],
+        responsive:{
+          0:{
+            items:1
+          },
+          480:{
+            items:1
+          },
+          600:{
+            items:2
+          },
+          800:{
+            items:2
+          },
+          1024:{
+            items:2
+          },
+          1200:{
+            items:2
+          }
+        }
+    });
+
+    //owl-Carousel-ThreeColumn
+    $('.owl-column.owl-col-3').owlCarousel({
+        loop:true,
+        autoplay: false,
+        autoplayHoverPause:false,
+        smartSpeed: 700,
+        items: 3,
+        margin:30,
+        dots: true,
+        nav:true,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1
+            },
+            480: {
+                items: 1
+            },
+            748: {
+                items: 2
+            },
+            1000: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
+    });
+
+    //owl-Carousel-FourColumn
+    $('.owl-column.owl-col-4').owlCarousel({
+        loop:true,
+        autoplay: 5000,
+        autoplayHoverPause:true,
+        smartSpeed: 700,
+        items: 4,
+        margin:30,
+        dots: true,
+        nav:true,
+        navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+        ],
+        responsive: {
+            0: {
+                items: 1,
+                loop: true
+            },
+            480: {
+                items: 1,
+                loop: true
+            },
+            600: {
+                items: 2,
+                loop: true
+            },
+            1000: {
+                items: 3,
+                loop: true
+            },
+            1200: {
+                items: 4,
+                loop: true
+            }
+        }
+    });
+
+  // Isotope-jquery - http://www.9bitstudios.com/2013/04/jquery-isotope-tutorial/
+
+  // filter items on button click
+  function isotopeGallery() {
+    $('.isotopeFilter a').on('click', function(){
+      $('.isotopeFilter .current').removeClass('current');
+      $(this).addClass('current');
+      var selector = $(this).attr('data-filter');
+      $container.isotope({
+          filter: selector,
+          animationOptions: {
+              duration: 750,
+              easing: 'linear',
+              queue: false
+          }
+       });
+       return false;
+    });
+
+    // init Isotope
+    if ($('.isotopeContainer').length) {
+      var $container = $('.isotopeContainer');
+        $container.isotope({
+          filter: '*',
+          animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+          }
+      });
+    }
+  }
+  
+  //LightBox / Fancybox
+  $('.lightbox-image').fancybox();
+
+  // datePicker
+  $('.date-picker').datepicker();
+  
+  //Skill Progress Bar
+  $('.progress-item').each(function() {
+    $(this).find('.bar-progress').animate({
+      width: $(this).attr('data-percent')
+    },2000);
+  });
+
+  // Funfact Number Counter
+  function counterfunfact() {
+    var timer = $('.timer');
+    if(timer.length) {
+      timer.appear(function () {
+        timer.countTo();
+      });
+    }
+    }
+
+
+    
+  //Accordions
+  $('.accordion-style .acc-title').on('click', function() {
+  if($(this).hasClass('active')!==true){
+      $('.accordion-style .acc-title').removeClass('active');
+    }      
+  if ($(this).next('.acc-content').is(':visible')){
+      $(this).removeClass('active');
+      $(this).next('.acc-content').slideUp(500);
+    }
+  else{
+      $(this).addClass('active');
+      $('.accordion-style .acc-content').slideUp(500);
+      $(this).next('.acc-content').slideDown(500);  
+    }
+  });
+
+  // fullScreen div style
+  $('.fullScreen').css({'height':($(window).height())+'px'});
+  $(window).on('resize', function(){
+    $('.fullScreen').css({'height':($(window).height())+'px'});
+  });
+
+  // Div Topfixed style
+  if($('.div-topfixed').length){
+    var summaries = $('.div-topfixed');
+    summaries.each(function(i) {
+      var summary = $(summaries[i]);
+      var next = summaries[i + 1];
+
+      summary.scrollToFixed({
+        marginTop: $('.header-main').outerHeight(true) + 10,
+        limit: function() {
+          var limit = 0;
+          if (next) {
+            limit = $(next).offset().top - $(this).outerHeight(true) - 10;
+          }
+          return limit;
+        },
+        zIndex: 999
+      });
+    });
+  }
+  
+  // scroll to top btn
+
+    //Check to see if the window is top if not then display button
+    $(window).on('scroll', function(){
+      if ($(this).scrollTop() > 250) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
+    
+    //Click event to scroll to top
+    $('.scroll-to-top').on('click', function(){
+      $('html, body').animate({scrollTop : 0},800);
+      return false;
+    });
+  
+  // Elements Animation
+  if($('.wow').length){
+    var wow = new WOW(
+      {
+      boxClass:     'wow',
+      animateClass: 'animated',
+      offset:       0,
+      mobile:       true,
+      live:         true
+      }
+    );
+    wow.init();
+  }
+
+/* ==========================================================================
+   When document is ready, do
+   ========================================================================== */
+    
+
+
+  $(document).on('ready', function() {
+      counterfunfact();
+    });
+
+
+
+/* ==========================================================================
+   When document is Scrollig, do
+   ========================================================================== */
+  
+  $(window).on('scroll', function() {
+  });
+  
+/* ==========================================================================
+   When document is loading, do
+   ========================================================================== */
+  
+  $(window).on('load', function() {
+    stylePreloader();
+    isotopeGallery();
+  });
+  
+
+/* ==========================================================================
+   When Window is resizing, do
+   ========================================================================== */
+  
+  $(window).on('resize', function() {
+    ww = document.body.clientWidth;
+    adjustMenu();
+  });
+  
+
+})(window.jQuery);
+
+
