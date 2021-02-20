8e83e0b490f64f5bc34128845177764e2006e12d
rccgloveregina <63160811+rccgloveregina@users.noreply.github.com>
2021-02-20 16:38:17 +0000

    Update custom-rccgla.js

--- js/custom-rccgla.js
+++ js/custom-rccgla.js
@@ -427,8 +427,8 @@ function loadlatestsermon() {
                 function (data) {
                     var output;
                     $('#result-sermon').empty();
-             console.log(data.items.length)
-              console("notvalid");      
+             console.log(data.items.length);
+              console.log("notvalid");      
                     if (data.items.length == 0) {
                         $('#result-sermon').html("<p>There are no sermons to display at this time.</p>");
                     }
